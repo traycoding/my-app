cbca8e15cab1a7df297131b5d46d2a849dbdaadc
sengtray <traycoding@gmail.com>
2023-03-10 22:46:36 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>hello my name is vy sengtray</h1>
+    </>
+  );
+}
+
+export default App;
